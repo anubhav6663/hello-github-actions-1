0ac33e7b4a3439e7698a948b7bc40ebe6763af9a
anubhav@infracloud.io <anubhav@infracloud.io>
2019-09-19 14:38:48 +0530

    Testing

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -l
 
-sh -c "git pull origin master"
-sh -c "cat action-a/entrypoint.sh"
-sh -c "git checkout -b test1"
-sh -c "cat action-a/entrypoint.sh"
+# branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+# bname=$(echo $branch | awk '{n=split($0,a); print a[n]}') 
+# branch=$(echo $bname | awk '{split($0,b,")");print b[1]}')
+# echo $branch
+commitID=$(git log --format="%H" -n 1)
+echo $commitID
+git checkout master 
+git cherry-pick $commitID
+#git push origin master
